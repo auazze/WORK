f853b03bce4e83b5327d29f7e300cd4e5dfaf3bc
Степан Станкевич <auazze@plllc.ru>
2025-06-04 15:37:11 +0500

    Third day of work

--- index.php
+++ index.php
@@ -6,9 +6,39 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="/css/custom.css">
 </head>
 <body>
-<?php include_once("navbar.php") ?>
+<div class="header">
+    <img src="header-image.jpg" class="header-image" alt="">
+
+    <div class="container-lg">
+        <div class="row">
+            <div class="col">
+                <?php include("navbar.php") ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <h1>
+                    
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-lg">
+    <div class="row">
+        <div class="col">
+            Продающие сайты «под ключ»
+
+        </div>
+    </div>
+</div>
 </body>
 </html>
